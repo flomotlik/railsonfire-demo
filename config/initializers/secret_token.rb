a40365aff639c6cfe2f3c7f50d95b7cd03a5ab16
Florian Motlik <flomotlik@gmail.com>
2011-10-10 19:45:41 +0200

    initial
    Signed-off-by: Florian Motlik <flomotlik@gmail.com>

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KopenhagenRocks::Application.config.secret_token = 'e80ad36a48d20158028d59deb1455cab71de539514acd2a16050b87c0094f0d778b965a93ec7a375b177035580893f4551ac1ebf9542c6cd0304bf0218a93a23'
